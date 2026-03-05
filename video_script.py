76ea7f366aa0591cc923fcf1786ee80eb904eb50
kcgitbit <kokchaums@gmail.com>
2026-03-05 16:07:59 +0800

    Create video_script.py

---
+++ video_script.py
@@ -0,0 +1,17 @@
+def create_video_script(product):
+
+    script = f"""
+    Hook: This product is going viral!
+
+    Problem:
+    People struggle with this daily.
+
+    Solution:
+    {product} solves it instantly.
+
+    Call to action:
+    Link in bio.
+    """
+
+    return script
+
